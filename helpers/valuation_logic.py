3f47d8bf86e5cd084154ce0c8baab5c1f3575567
mgoelbu <156374400+mgoelbu@users.noreply.github.com>
2025-04-18 21:06:25 -0400

    Update valuation_logic.py

--- helpers/valuation_logic.py
+++ helpers/valuation_logic.py
@@ -2,6 +2,8 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import streamlit as st
 
+import yfinance as yf
+
 def analyze_valuation(ticker_symbol, peers):
     all_pe_ratios = {}
     eps = None
@@ -16,8 +18,14 @@ def analyze_valuation(ticker_symbol, peers):
         if pe is not None and pe > 0:
             all_pe_ratios[t] = pe
 
-    industry_pe_avg = sum(all_pe_ratios.values()) / len(all_pe_ratios)
+    pe_values = list(all_pe_ratios.values())
+    industry_pe_avg = sum(pe_values) / len(pe_values)
+    min_pe = min(pe_values)
+    max_pe = max(pe_values)
+
     implied_price = industry_pe_avg * eps if eps else None
+    implied_price_min = min_pe * eps if eps else None
+    implied_price_max = max_pe * eps if eps else None
     current_price = yf.Ticker(ticker_symbol).history(period="1d")['Close'][-1]
 
     if implied_price:
@@ -34,10 +42,15 @@ def analyze_valuation(ticker_symbol, peers):
         "peers": all_pe_ratios,
         "eps": eps,
         "industry_pe_avg": industry_pe_avg,
+        "min_pe": min_pe,
+        "max_pe": max_pe,
         "implied_price": implied_price,
+        "implied_price_min": implied_price_min,
+        "implied_price_max": implied_price_max,
         "current_price": current_price,
         "recommendation": decision
     }
+
 
 def plot_peers(peers_dict, highlight):
     fig, ax = plt.subplots()
