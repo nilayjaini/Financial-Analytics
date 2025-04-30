4a94d2c13cb3f570a297cd2e26f372c1a077d00a
Nilay Jaini <nilayjaini01@gmail.com>
2025-04-30 14:53:22 -0400

    Update complete_page.py

--- pages/complete_page.py
+++ pages/complete_page.py
@@ -327,10 +327,10 @@ with tab2:
         try:
     # Get relevant price data
             eps_2024 = eps_data.loc[idx, 2024] if 2024 in eps_data.columns else None
-            median_pe_array = gsubind_to_median_pe.get(gsubind_data[idx], [None] * len(years))
-            median_pe_2024 = None
-            if median_pe_array is not None and len(median_pe_array) > 2024:
-                median_pe_2024 = median_pe_array[2024]
+            median_pe_array = gsubind_to_median_pe.get(gsubind_data[idx], [] )
+            median_pe_2024 = median_pe_array[-1] if median_pe_array else None
+            # if median_pe_array is not None and len(median_pe_array) > 2024:
+            #     median_pe_2024 = median_pe_array[2024]
             st.write("EPS 2024:", eps_2024)
             st.write("Median P/E Array:", median_pe_array)
             st.write("Median P/E 2024:", median_pe_2024)
